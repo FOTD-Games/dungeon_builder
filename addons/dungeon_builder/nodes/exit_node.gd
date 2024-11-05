89117f7354eadbf0400d86ba933a439ac1248fa7
zendarva <zendarva@gmail.com>
2024-11-05 14:23:58 -0600

    Adding directional indicator to exit_node

--- addons/dungeon_builder/nodes/exit_node.gd
+++ addons/dungeon_builder/nodes/exit_node.gd
@@ -6,17 +6,28 @@ class_name exit_node
 var col_area:CollisionShape2D
 
 var texture_rect: TextureRect
+var arrow_texture: TextureRect
 
 @export
 var size:=Vector2(32,32)
 
 enum directions {LEFT, RIGHT, UP, DOWN}
+
 var dirmap :=[Vector2.LEFT, Vector2.RIGHT,Vector2.UP,Vector2.DOWN]
 
 @export
-var direction:directions
+var direction:directions:
+	set(value):
+		direction=value;
+		dirty=true;
+
 @export
-var door:bool=false
+var door:bool=false:
+	set(value):
+		door=value;
+		dirty=true
+
+var dirty:bool = false
 
 var connected:=false
 
@@ -24,36 +35,77 @@ var atlas := preload("res://addons/dungeon_builder/nodes/exit_atlas.tres") as At
 
 @export
 var matching_tag_sets:Exit_Node_Want_Tag
+
+func _ready() -> void:
+	setup()
 	
 func setup() ->void:
 	if get_child_count() == 0:
 		texture_rect=TextureRect.new()
 		col_area = CollisionShape2D.new()		
+		arrow_texture=TextureRect.new()
+		
 		add_child(col_area)
-		col_area.owner= get_tree().edited_scene_root
+		#col_area.owner= get_tree().edited_scene_root
 		add_child(texture_rect)
-		texture_rect.owner= get_tree().edited_scene_root
-		
-		
-		texture_rect.texture=atlas
-		texture_rect.size=size
-		texture_rect.position-=size/2
-		atlas.region.size=Vector2(16,16)
-		
-		atlas.region.position=Vector2(16,0)
-
+		#texture_rect.owner= get_tree().edited_scene_root
+		add_child(arrow_texture)
+		#arrow_texture.owner=get_tree().edited_scene_root
+		dirty=true		
 
 func get_direction() ->Vector2:
 	return dirmap[direction]
 
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
 		setup()
+	if dirty:
+		if arrow_texture == null:
+			for child in get_children():
+				child.queue_free()
+				remove_child(child)
+			setup()
+			
+		col_area.shape=RectangleShape2D.new()
+		col_area.shape.set_size(size)
+		#col_area.visible=true
+
+		arrow_texture.texture=atlas.duplicate()
+		arrow_texture.size=size
+		arrow_texture.position=-size/2
+		arrow_texture.texture.region.size=Vector2(16,16)
+		arrow_texture.texture.region.position=Vector2(32,0)
+		arrow_texture.pivot_offset = size/2
+		arrow_texture.self_modulate.a=160
+		
+		texture_rect.texture=atlas.duplicate()
+		texture_rect.size=size
+		texture_rect.position=-size/2
+		texture_rect.texture.region.size=Vector2(16,16)
+		texture_rect.texture.region.position=Vector2(16,0)
+		
+		dirty=false;
 
 func _physics_process(delta: float) -> void:
-		if door:
-			atlas.region.position=Vector2(0,0)
-		else:
-			atlas.region.position=Vector2(16,0)
+	if texture_rect == null:
+		dirty=true
+	if dirty:
+		return;
+	if door:
+		texture_rect.texture.region.position=Vector2(0,0)
+	else:
+		texture_rect.texture.region.position=Vector2(16,0)
+
+	match(direction):
+		directions.LEFT:
+			arrow_texture.rotation=0
+		directions.RIGHT:
+			arrow_texture.rotation=PI
+		directions.UP:
+			arrow_texture.rotation=PI/2
+		directions.DOWN:
+			arrow_texture.rotation=PI+PI/2
+		
 
 func compare_direction(other:exit_node) ->bool:
 	match(direction):
